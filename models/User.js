1a42b64e8189ef26b17bd53f055184f9499bff04
Rujaxx <shaikhrujan@gmail.com>
2021-08-06 11:40:57 +0530

    Custom error handler

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { nextTick } = require('process');
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -37,7 +39,10 @@ const UserSchema = new mongoose.Schema({
 
 
 //encrypt password
-UserSchema.pre('save' , async function() {
+UserSchema.pre('save' , async function(next) {
+    if(!this.isModified('password')){
+        next();
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 })
@@ -53,5 +58,21 @@ UserSchema.methods.getSignedJwtToken = function () {
 UserSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword,this.password)
     }
+
+//Generate and Hash Password
+UserSchema.methods.getResetPasswordToken = function () {
+    //generate token
+    const resetToken = crypto.randomBytes(20).toString('hex')
+
+    // hash token and set to resetPasswordToken Field
+    this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex')
+
+    //set expire
+    this.resetPasswordExpire = Date.now()+ 10 * 60 * 1000;
+    return resetToken
+}
 
 module.exports = mongoose.model('User', UserSchema)
